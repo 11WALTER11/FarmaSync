ba061b1e40847c465931bdc5e728ec9b39e5d2dc
WALTER <arish.gzp988@gmail.com>
2023-11-05 11:25:25 +0530

    link styling removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // App.js
 import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
