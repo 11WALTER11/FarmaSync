afcb5276ce7b68f10386ead956110a049f09faa7
WALTER <arish.gzp988@gmail.com>
2023-11-24 19:36:12 +0530

    sensor added

---
+++ SensorSimulation/script.py
@@ -0,0 +1,62 @@
+import boto3
+import json
+import random
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region = os.getenv("AWS_REGION")
+iot_endpoint = os.getenv("IOT_ENDPOINT")
+iot_topic = os.getenv("IOT_TOPIC")
+
+iot_client = boto3.client(
+    'iot-data',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=aws_region,
+    endpoint_url=f'https://{iot_endpoint}'
+)
+
+def simulate_sensor_data():
+    # Simulate NPK sensor data
+    nitrogen = random.uniform(0, 100)
+    phosphorus = random.uniform(0, 100)
+    potassium = random.uniform(0, 100)
+
+    # Simulate soil moisture sensor data
+    soil_moisture = random.uniform(0, 100)
+
+    # Simulate humidity sensor data
+    humidity = random.uniform(0, 100)
+
+    sensor_data = {
+        'nitrogen': nitrogen,
+        'phosphorus': phosphorus,
+        'potassium': potassium,
+        'soil_moisture': soil_moisture,
+        'humidity': humidity
+    }
+
+    return sensor_data
+
+def publish_sensor_data():
+    while True:
+        sensor_data = simulate_sensor_data()
+
+        # Publish data to IoT Core
+        payload = json.dumps(sensor_data)
+        response = iot_client.publish(
+            topic=iot_topic,
+            payload=payload
+        )
+
+        print(f"Published data: {payload}")
+
+        time.sleep(5)  # Adjust the sleep duration based on your requirements
+
+if __name__ == "__main__":
+    publish_sensor_data()
