05fa9162b62dd0d136c07c9bac6a2175d8208005
WALTER <arish.gzp988@gmail.com>
2023-12-01 19:50:03 +0530

    chatgpt integrated

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config as dotenvConfig } from "dotenv";
 import AWS from "aws-sdk";
 import cors from "cors";
+import axios from "axios";
 
 dotenvConfig();
 
@@ -18,44 +19,96 @@ AWS.config.update({ region });
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 // Define a function to retrieve data from DynamoDB
-const retrieveMostRecentSensorData = (res) => {
-  // Define the parameters for the scan (retrieve all items in the table)
-  const params = {
-    TableName: "SensorData", // Replace with your table name
-  };
+const retrieveMostRecentSensorData = async () => {
+  return new Promise((resolve, reject) => {
+    // Define the parameters for the scan (retrieve all items in the table)
+    const params = {
+      TableName: "SensorData", // Replace with your table name
+    };
 
-  // Scan DynamoDB
-  docClient.scan(params, (err, data) => {
-    if (err) {
-      console.error("Error reading from DynamoDB:", err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      // Sort data based on sample_time in descending order (most recent first)
-      data.Items.sort((a, b) => b.sample_time - a.sample_time);
-
-      // Retrieve only the most recent item
-      const mostRecentItem = data.Items.length > 0 ? data.Items[0] : null;
-
-      if (mostRecentItem) {
-        // Convert epoch time to a readable format
-        const formattedTime = new Date(
-          mostRecentItem.sample_time * 1000
-        ).toISOString();
-        mostRecentItem.sample_time_formatted = formattedTime;
-
-        console.log("Most recent item:", mostRecentItem);
-        res.json(mostRecentItem); // Send the most recent item as a JSON response
+    // Scan DynamoDB
+    docClient.scan(params, (err, data) => {
+      if (err) {
+        console.error("Error reading from DynamoDB:", err);
+        reject("Internal Server Error");
       } else {
-        console.log("No data found");
-        res.status(404).send("No data found");
+        // Sort data based on sample_time in descending order (most recent first)
+        data.Items.sort((a, b) => b.sample_time - a.sample_time);
+
+        // Retrieve only the most recent item
+        const mostRecentItem = data.Items.length > 0 ? data.Items[0] : null;
+
+        if (mostRecentItem) {
+          // Convert epoch time to a readable format
+          const formattedTime = new Date(
+            mostRecentItem.sample_time * 1000
+          ).toISOString();
+          mostRecentItem.sample_time_formatted = formattedTime;
+
+          console.log("Most recent item:", mostRecentItem);
+
+          resolve(mostRecentItem); // Resolve with the most recent item
+        } else {
+          console.log("No data found");
+          reject("No data found");
+        }
       }
-    }
+    });
   });
 };
 
+// Define a function to send data to Palm2 API
+const sendToPalm2API = async (sensorData) => {
+  const palm2ApiUrl = `https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=${process.env.PALM_API_KEY}`;
+
+  const axiosConfig = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const requestBody = {
+    prompt: {
+      text: `Provide me an action plan based on this sensor data only (sepraate each points by /): ${JSON.stringify(
+        sensorData
+      )}`,
+    },
+  };
+
+  return await axios.post(palm2ApiUrl, requestBody, axiosConfig);
+};
+
+// Define a new route for Palm2 data
+app.get("/palm2-data", async (req, res) => {
+  try {
+    // Retrieve most recent sensor data
+    const mostRecentSensorData = await retrieveMostRecentSensorData();
+
+    // Send the most recent sensor data to Palm2 API
+    const palm2ApiResponse = await sendToPalm2API(mostRecentSensorData);
+
+    // You can handle the response from Palm2 API here
+
+    // Send the combined response to the client
+    res.json({
+      sensorData: mostRecentSensorData,
+      palm2ApiResponse: palm2ApiResponse.data,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // Define a route to manually trigger data retrieval (for testing)
-app.get("/sensor-data/", (req, res) => {
-  retrieveMostRecentSensorData(res);
+app.get("/sensor-data", async (req, res) => {
+  try {
+    const mostRecentSensorData = await retrieveMostRecentSensorData();
+    res.json(mostRecentSensorData);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Start the server
--- src/components/ActionPlan.jsx
+++ src/components/ActionPlan.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ActionPlan = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/palm2-data");
+      const palm2Data = response.data.palm2ApiResponse;
+      setData(palm2Data);
+    } catch (error) {
+      setError("Error fetching data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array ensures useEffect runs only once on component mount
+
+  const handleGenerateNewResults = () => {
+    setLoading(true);
+    fetchData();
+  };
+
   return (
     <>
       <div className="container d-flex justify-content-start p-4">
         <div className="bg-light col-12" style={{ height: "400px" }}>
           <h3 className="text-center">Action Plan</h3>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error}</p>}
+          {data && (
+            <>
+              <ul>
+                {/* Display each action as a separate list item */}
+                {data.candidates[0].output.split(". ").map((action, index) => (
+                  <li key={index}>{action}</li>
+                ))}
+              </ul>
+              <button onClick={handleGenerateNewResults}>
+                Generate New Results
+              </button>
+            </>
+          )}
         </div>
       </div>
     </>
